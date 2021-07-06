6a3e19378c077cd0b38c4d49f26a35b28cd53866
hsilvioviana <hsilvioviana@hotmail.com>
2021-07-06 14:41:14 -0300

    Funcionalide Login Feita

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,53 @@
+import axios from "axios"
+import { useState } from "react"
+import { useHistory } from "react-router"
 import useUnprotectPage from "../../hooks/useUnprotectPage"
-
+import { baseUrl } from "../../parameters"
+import { goToHome, goToSignup } from "../../routes/coordinator"
 
 function Login() {
 
     useUnprotectPage()
 
+    const history = useHistory()
+
+    const loginForm = { login: "", password: "" }
+
+    const [form, setForm] = useState(loginForm)
+
+    const onChange = (event) => {
+
+        const {name, value} = event.target
+        setForm({...form, [name]: value})
+    }
+
+    const login = async (event) => {
+
+        event.preventDefault()
+
+        try {
+
+            const response = await axios.post(`${baseUrl}/users/login`, form)
+            window.localStorage.setItem("token", response.data.token)
+            goToHome(history)
+        }
+        catch (error) {
+
+            setForm({ ...loginForm, login: form.login })
+            window.alert(error.response.data.error)
+        }
+    }
+
     return (
-        <div>Login</div>
+        <div>
+            <h1>Login</h1>
+            <form>
+                <input onChange={onChange} placeholder="Login" name="login" value={form.login}/>
+                <input onChange={onChange} placeholder="Senha" name="password" value={form.password} type="password"/>
+                <button onClick={login}>Entrar</button>
+            </form>
+            <button onClick={() => goToSignup(history)}>Cadastro</button>
+        </div>
     )
 }
 
